479b5c72e689b44dd1c32d5c5d1992dcc46fe2e1
Subhashish Naik <subhashishnaik1@gmail.com>
2022-07-02 13:58:13 +0530

    2/7/22

--- LinkedList/src/Palindromic.java
+++ LinkedList/src/Palindromic.java
@@ -1,6 +1,54 @@
+// LinkedList is Palindrome or not
 import java.util.*;
-
+import java.io.*;
 public class Palindromic {
+
+    static Node middle(Node head){
+        if(head == null || head.next == null)
+            return head;
+        Node slow = head, fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+    static Node reverse(Node head){
+        if(head == null || head.next == null)
+            return head;
+
+        Node prev = null, curr = head, nxt;
+
+        while(curr != null){
+            nxt = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+
+        return prev;
+    }
+    static Boolean isPalindrome(Node head){
+        if(head == null || head.next == null)
+            return true;
+
+        Node temp = middle(head);
+        Node mid = temp.next;
+        temp.next = null;
+        mid = reverse(mid);
+
+        while(head != null && mid != null){
+            if(head.data != mid.data)
+                return false;
+            head = head.next;
+            mid = mid.next;
+        }
+
+        return true;
+    }
     public static void main(String[] args) {
         Node head = null;
         Scanner sc = new Scanner(System.in);
@@ -11,6 +59,18 @@ public class Palindromic {
         for(int i=0;i<n;i++)
             ar[i] = sc.nextInt();
 
+        head = new Node(ar[0]);
+        Node temp = head;
+        for(int i=1;i<n;i++){
+            Node nn = new Node(ar[i]);
+            temp.next = nn;
+            temp = temp.next;
+        }
+
+        if(isPalindrome(head))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
 
     }
 }
